4b9925b5246fc772ad67019fe8933b2f7d20d36c
AntonKtrue <kaa-work@yandex.ru>
2017-07-04 10:28:29 +0300

    Update Experiment.java

--- Experiment.java
+++ Experiment.java
@@ -1,22 +1,22 @@
-import java.util.LinkedList;
-
 public class Experiment {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         LinkedList<Integer> l = new LinkedList<>();
+
         for(int i = 0 ; i < 1_000_000 ; i++)
         {
             l.add(i);
         }
-
+        ArrayList<Integer> l2 = new ArrayList<>(l.size()+500);
+        l2.addAll(l);
+        ArrayList<Integer> l3 = new ArrayList<>(500);
         int center = l.size() >> 1;
-        System.out.println(center);
-
         for (int i = 0; i < 500 ; i++)
         {
-            l.add(center, 500-i);
+            l3.add(i);
         }
-        for (int i = center - 10; i < center + 520; i++) {
-            System.out.println(l.get(i).toString());
-        }
+        l2.addAll(center,l3);
+        l = new LinkedList<>(l2);
+        System.out.println( (System.currentTimeMillis() - start) +" ms");
     }
 }
